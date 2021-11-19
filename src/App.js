a844239241c643668a251c38dfe896c86a964949
alvaromoca1 <alvaromoca1@gmail.com>
2021-11-19 18:26:00 -0500

    login for spotify

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Login from './pages/login/Login';
+import { getTokenFromUrl } from './configs/spotify';
+import SpotifyWebApi from "spotify-web-api-js";
+
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+  const [token, setToken] = useState();
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+      spotify.setAccessToken(_token);
+    }
+
+    console.log("token", token);
+  }, []);
+
   return (
-    <div className="App"></div>
+    <div className="app">{token ? <h1>Logged in</h1> : <Login />}</div>
   );
 }
 
